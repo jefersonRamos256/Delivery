d927df7ee1f63766844423ea5caca66b6b7e2cff
Brainly <jefersonoliveiraramos8@gmail.com>
2019-11-05 13:40:10 -0300

    Primeiro Commit

---
+++ app/src/main/java/com/example/delivery/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.delivery;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
